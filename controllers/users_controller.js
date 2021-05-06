e345561a176287b8d34a782f5509b3887b804680
sahilsaini137 <sahilsaini137@gmail.com>
2021-05-06 23:02:06 +0530

    create view and forms for sign up and sign in

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,4 +2,27 @@ module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title: "User Profile"
     });
+};
+
+// render the sign up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: "Code Forum | Sign Up"
+    })
+}
+// render the sign in page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: "Code Forum | Sign In"
+    })
+}
+
+// get the sign up data
+module.exports.create = function(req, res){
+    // Todo later
+}
+
+// Sign in and create session for user
+module.exports.createSession = function(req, res){
+    // todo later
 }
