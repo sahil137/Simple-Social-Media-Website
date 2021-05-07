cba5772bd02b959bcd719ab207c53963686371ba
sahilsaini137 <sahilsaini137@gmail.com>
2021-05-07 23:09:10 +0530

    add mongo store to store user session

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -46,5 +46,25 @@ passport.deserializeUser(function(id, done){
     });
 });
 
+// check if user is authenticated
+
+passport.checkAuthentication = function(req, res, next){
+    //  if the user is signed in then pass on request to the next function
+    if (req.isAuthenticated()){
+        return next();
+    }
+    // if user is not signed in
+    return res.redirect('/users/sign-in')
+}
+
+
+passport.setAuthenticatedUser = function(req, res, next){
+    if (req.isAuthenticated()){
+        // req.user contains the current signed in user from session cookie
+        res.locals.user = req.user;
+    }
+
+    next();
+}
 
 module.exports = passport;
--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 
+const MongoStore = require('connect-mongodb-session')(session);
+
 app.use(express.urlencoded());
 
 app.use(cookieParser());
@@ -26,7 +28,7 @@ app.set('layout extractScripts', true);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
+// mongo store is used to store the session cookie in the db
 app.use(session({
     name: 'code-forum',
     // todo change secret before deployment
@@ -35,11 +37,22 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 10)
-    }
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        },
+        function(err){
+            console.log(err || 'connect-mongodb setup ok');
+        }
+    )
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
 
 // Use express router
 app.use('/', require('./routes'));
