aa6f5fa3041026f7399abfa9f6669b46d6b4c491
sahilsaini137 <sahilsaini137@gmail.com>
2021-05-11 02:29:13 +0530

    add comment controller and route

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ const homeController = require('../controllers/home_controller');
 router.get('/', homeController.home);
 router.use('/users', require('./users')); // redirect path to users in same folder
 router.use('/post', require('./post')); // redirect path to post
+router.use('/comments', require('./comments'));
 
 module.exports = router;
