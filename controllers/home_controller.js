3968fa7ac68865735a18ef44f8cfc4f0776d1a7b
sahilsaini137 <sahilsaini137@gmail.com>
2021-05-13 01:26:43 +0530

    add user profile links at the home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,5 @@
 const Post = require('../models/post');
-
+const User = require('../models/user');
 module.exports.home = function(req, res){
     // Post.find({}, function(err, post){
     //     return res.render('home', {
@@ -16,9 +16,12 @@ module.exports.home = function(req, res){
         }
     })
     .exec(function(err, post){
-        return res.render('home', {
-            title: "Code Forum | Home",
-            posts: post
+        User.find({}, (err, users) => {
+            return res.render('home', {
+                title: "Code Forum | Home",
+                posts: post,
+                all_users: users
+            });
         });
     });
 };
