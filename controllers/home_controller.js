ea175cc244e4712d1aca9c8142fc6bd537e65677
Sahil Saini <46193454+sahil137@users.noreply.github.com>
2021-05-14 01:18:35 +0530

    convert home_controller to async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,27 +1,26 @@
 const Post = require('../models/post');
 const User = require('../models/user');
-module.exports.home = function(req, res){
-    // Post.find({}, function(err, post){
-    //     return res.render('home', {
-    //         title: "Code Forum | Home",
-    //         posts: post
-    //     });
-    // });
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, post){
-        User.find({}, (err, users) => {
-            return res.render('home', {
-                title: "Code Forum | Home",
-                posts: post,
-                all_users: users
-            });
+module.exports.home = async function(req, res){
+    try{
+        let post = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
         });
-    });
+    
+        let users = await User.find({});
+    
+        return res.render('home', {
+            title: "Code Forum | Home",
+            posts: post,
+            all_users: users
+        });
+    }
+    catch(err){
+        console.log(`Error: ${err}`);
+    }
+
 };
