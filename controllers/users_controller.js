c88777e072da2e94ed118be16e293bea9ae28178
Sahil Saini <46193454+sahil137@users.noreply.github.com>
2021-05-22 18:52:54 +0530

    install multer and save user avatar

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -9,13 +9,34 @@ module.exports.profile = function(req, res){
     });
 };
 
-module.exports.update = (req, res) => {
+module.exports.update = async (req, res) => {
     if (req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id, req.body, (err, user) => {
+        try{
+            let user = await User.findById(req.params.id);
+            User.uploadedAvatar(req, res, function(err){
+                if (err){
+                    console.log('**Multer Error', err);
+                }
+
+                user.name = req.body.name,
+                user.email = req.body.email
+
+                if (req.file){
+                    user.avatar = User.avatarPath + '/' + req.file.filename
+                }
+
+                user.save();
+                return res.redirect('back');
+
+            });
+        }
+        catch(err){
+            req.flash('error', err);
             return res.redirect('back');
-        });
+        }
     }
-    else {
+    else{
+        req.flash('error', 'Unauthorized');
         return res.status(401).send('Unauthorized');
     }
 }
