a846693b31964ddde5b979e3ebabde24c6812471
Sahil Saini <46193454+sahil137@users.noreply.github.com>
2021-05-23 00:35:51 +0530

    show user avatar and replace updated avatar

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const fs = require('fs');
+const path = require('path');
 
 module.exports.profile = function(req, res){
     User.findById(req.params.id, (err, user) => {
@@ -22,6 +24,11 @@ module.exports.update = async (req, res) => {
                 user.email = req.body.email
 
                 if (req.file){
+
+                    if (user.avatar){
+                        fs.unlinkSync(path.join(__dirname, '..', user.avatar));
+                    }
+
                     user.avatar = User.avatarPath + '/' + req.file.filename
                 }
 
